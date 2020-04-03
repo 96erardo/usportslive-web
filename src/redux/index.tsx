be13c987f516cf914c0f536de4c34543757aaddf
GerardoAGL96 <gerardoagl96@gmail.com>
2020-04-02 20:53:21 -0400

    Initial redux async configuration

--- src/redux/index.tsx
+++ src/redux/index.tsx
@@ -1,6 +1,9 @@
-import { createStore } from 'redux';
-import { rootReducer } from './reducers'
+import thunkMiddleware from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import { rootReducer } from './reducers';
 
-const store = createStore(rootReducer, {});
+const store = createStore(rootReducer, {}, applyMiddleware(
+  thunkMiddleware
+));
 
 export default store;
