166ef2a8333ef93fca950c8498a68d6f0afd76f2
GerardoAGL96 <gerardoagl96@gmail.com>
2020-04-29 22:03:02 -0400

    Material ui installation and theme customization

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { 
-  BrowserRouter as Router,
-  Route,
-  Switch
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from '../redux';
 import Home from './pages/Home';
 import { loadAppResources } from '../redux/actions/app';
 import AuthCallback from './pages/AuthCallback';
 import { AppDispatch } from '../shared/types';
 import AppLoader from './organisms/AppLoader';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
+import theme from '../config/theme';
+import store from '../redux';
 
 function App () {
   const [loading, setLoading] = useState(true);
@@ -27,14 +26,17 @@ function App () {
 
   return (
     <Provider store={store}>
-      <AppLoader loading={loading} error={error}>
-        <Router>
-          <Switch>
-            <Route exact path="/oauth/callback" component={AuthCallback}/>
-            <Route exact path="/" component={Home}/>
-          </Switch>
-        </Router>
-      </AppLoader>
+      <ThemeProvider theme={theme}>
+        <AppLoader loading={loading} error={error}>
+          <Router>
+            <Switch>
+              <Route exact path="/oauth/callback" component={AuthCallback}/>
+              <Route exact path="/" component={Home}/>
+            </Switch>
+          </Router>
+        </AppLoader>
+        <CssBaseline />
+      </ThemeProvider>
     </Provider>
   );
 }
---
+++ src/shared/assets/fonts/index.tsx
@@ -0,0 +1,31 @@
+import './Muli-Regular.ttf';
+import './Muli-Italic.ttf';
+import './Muli-ExtraBold.ttf';
+import './Muli-ExtraBoldItalic.ttf';
+
+export const muli = [
+  {
+    fontFamily: 'Muli',
+    fontStyle: 'normal',
+    fontWeight: 400,
+    src: `url(./Muli-Regular.ttf) format('truetype')`,
+  },
+  {
+    fontFamily: 'Muli',
+    fontStyle: 'italic',
+    fontWeight: 400,
+    src: `url(./Muli-Italic.ttf) format('truetype')`,
+  },
+  {
+    fontFamily: 'Muli',
+    fontStyle: 'normal',
+    fontWeight: 700,
+    src: `url(./Muli-ExtraBold.ttf) format('truetype')`,
+  },
+  {
+    fontFamily: 'Muli',
+    fontStyle: 'italic',
+    fontWeight: 700,
+    src: `url(./Muli-ExtraBoldItalic.ttf)  format('truetype')`,
+  }
+];
